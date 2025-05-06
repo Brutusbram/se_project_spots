6349bf8563f98cf26f6cb120b766f753c7b73daa
AlexBramwell <Brutusbram@gmail.com>
2025-05-05 17:59:35 -0700

    sprint 4 stage 3 v1

--- scripts/index.js
+++ scripts/index.js
@@ -3,11 +3,31 @@ const editProfileModal = document.querySelector("#edit-profile-modal");
 const closeEditProfileButton = editProfileModal.querySelector(
   ".modal__close-button"
 );
+const editProfileNameInput =
+  editProfileModal.querySelector("#modal__name-input");
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#modal__description-input"
+);
+
 const addPostButton = document.querySelector(".profile__add");
 const newPostModal = document.querySelector("#new-post-modal");
 const closeNewPostButton = newPostModal.querySelector(".modal__close-button");
+const profileNameElement = document.querySelector(".profile__name");
+const profileDescriptionElement = document.querySelector(
+  ".profile__description"
+);
+const editSubmitButton = editProfileModal.querySelector(
+  ".modal__submit-button"
+);
+const addCardFormElement = document.querySelector("#new-post-modal");
+const nameInput = newPostModal.querySelector(
+  "#card-caption-input"
+);
+const linkInput = newPostModal.querySelector("#card-image-input");
 
 editProfileButton.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameElement.textContent;
+  editProfileDescriptionInput.value = profileDescriptionElement.textContent;
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -22,3 +42,19 @@ addPostButton.addEventListener("click", function () {
 closeNewPostButton.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleeditSubmitButton(evt) {
+  evt.preventDefault();
+  profileNameElement.textContent = editProfileNameInput.value;
+  profileDescriptionElement.textContent = editProfileDescriptionInput.value;
+  editProfileModal.classList.remove("modal_is-opened");
+}
+editProfileModal.querySelector(".modal__form").addEventListener("submit", handleeditSubmitButton);
+
+function handleAddCardFormSubmit(evt) {
+  evt.preventDefault();
+  console.log(nameInput.value);
+  console.log(linkInput.value);
+  newPostModal.classList.remove("modal_is-opened");
+}
+addCardFormElement.addEventListener("submit", handleAddCardFormSubmit);
