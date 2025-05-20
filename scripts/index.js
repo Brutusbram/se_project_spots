1a00eee2f8c1155af31f6c558092e522cdb4021c
AlexBramwell <Brutusbram@gmail.com>
2025-05-19 21:11:30 -0700

    project 5 stage 1 v1

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,24 @@
+const initialCards = [
+  {name: "Val Thorens",
+    link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"
+   },
+     {name: "restaurant Terrace",
+    link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg"
+   },
+     {name: "Val A outdoor cafe",
+    link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg"
+   },
+     {name: "A very long bridge, over the forest and through the trees",
+    link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg"
+   },
+     {name: "Tunnel with morning light",
+    link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg"
+   },
+     {name: "Mountain House",
+    link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg"
+   }
+  ];
+
 const editProfileButton = document.querySelector(".profile__edit");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const closeEditProfileButton = editProfileModal.querySelector(
@@ -48,7 +69,7 @@ closeNewPostButton.addEventListener("click", function () {
   closeModal(newPostModal);
 });
 
-function handleeditSubmitButton(evt) {
+function handleEditSubmitButton(evt) {
   evt.preventDefault();
   profileNameElement.textContent = editProfileNameInput.value;
   profileDescriptionElement.textContent = editProfileDescriptionInput.value;
@@ -63,3 +84,8 @@ function handleAddCardFormSubmit(evt) {
   closeModal(newPostModal);
 }
 addCardFormElement.addEventListener("submit", handleAddCardFormSubmit);
+
+initialCards.forEach(function(item)  {
+  console.log(item.name);
+  console.log(item.link);
+});
